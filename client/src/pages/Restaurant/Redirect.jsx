e294f648c16bdae483f2819574e0a4d3ecb5820a
Rohit Dhakane <rohitdhakane6@gmail.com>
2024-02-16 17:05:28 +0530

    Add(creatd route)

---
+++ client/src/pages/Restaurant/Redirect.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+function Redirect() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    navigate(`/restaurant/${id}/overview`);
+  });
+
+  return <div></div>;
+}
+
+export default Redirect;
