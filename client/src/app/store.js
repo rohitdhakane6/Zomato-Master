3d35fd105ac6d3ccc9214d9680aeceba08ce5902
Rohit Dhakane <rohitdhakane6@gmail.com>
2024-02-18 20:36:18 +0530

    update(ReviewSlice)

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import restaurantReducer, { fetchRestaurants, fetchRestaurantByID, searchRestaurants } from "../features/resturant/restaurantSlice";
 import imageReducer, { fetchImageURL } from "../features/image/imageSlice";
+import ReviewSlice,{getReviews,postReview} from '../features/Reviews/ReviewSlice';
 
 const store = configureStore({
   reducer: {
     restaurant: restaurantReducer,
     image: imageReducer,
+    review:ReviewSlice,
   },
 });
 
-export { fetchRestaurants, fetchRestaurantByID, searchRestaurants, fetchImageURL };
+export { fetchRestaurants, fetchRestaurantByID, searchRestaurants, fetchImageURL,getReviews,postReview };
 
 export default store;
